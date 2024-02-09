28bfd3a99e0d889f643a91745b03b90014b9820c
kmu-jungsy <135489568+kmu-jungsy@users.noreply.github.com>
2024-02-09 17:55:26 +0900

    Delete elevator-sys-main directory

--- elevator-sys-main/elevator/Check/System/GlobalState.py
+++
@@ -1,8 +0,0 @@
-import enum
-
-class State(enum.Enum):
-    MOVEDOWN = 0
-    MOVEUP = 1
-    ACTIVE = 2
-    INACTIVE = 3
-    HOLDING = 4
--- elevator-sys-main/elevator/ElevatorSystemCheck.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from Check.StrategyCheck import *
-from Check.CabinetCheck import *
-from Check.SystemManagerCheck import *
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
