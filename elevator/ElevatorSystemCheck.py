b97e1ec8e24dea41250cbfffdb17a49435b6bfdb
kmu-jungsy <135489568+kmu-jungsy@users.noreply.github.com>
2024-02-09 17:56:03 +0900

    Add files via upload

---
+++ elevator/ElevatorSystemCheck.py
@@ -0,0 +1,7 @@
+import unittest
+from Check.StrategyCheck import *
+from Check.CabinetCheck import *
+from Check.SystemManagerCheck import *
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
